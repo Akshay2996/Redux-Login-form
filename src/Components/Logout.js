c01ca5d66da15ccdded118b164bc9e44b18b5459
Akshay Sharma <akshaysharmaay@gmail.com>
2022-07-02 19:15:31 +0530

    Logout component

---
+++ src/Components/Logout.js
@@ -0,0 +1,10 @@
+export const Logout = () => {
+  return (
+    <div className="logout">
+      <h1>
+        Welcome <span className="username">Akshay</span>
+      </h1>
+      <button className="logout__btn">Logout</button>
+    </div>
+  );
+};
